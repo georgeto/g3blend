da053bb27bf6f348e05859e03e262414a8103258
georgeto <georgeto@mailbox.org>
2024-03-22 23:24:05 +0100

    Refactor xmot import

--- g3blend/operators/io_import_xmot.py
+++ g3blend/operators/io_import_xmot.py
@@ -1,4 +1,5 @@
 import math
+from dataclasses import dataclass
 from pathlib import Path
 
 import bpy
@@ -11,6 +12,25 @@ from ..util import bone_correction_matrix, bone_correction_matrix_inv, calc_arm_
     read_genome_file, to_blend_quat, to_blend_vec
 
 logger = logging.getLogger(__name__)
+
+
+@dataclass
+class _ImportState:
+    arm_obj: bpy.types.Object
+    action: bpy.types.Action
+    fps: float
+    root_scale: float
+    root_matrix_no_scale: Matrix
+    min_frame_time: float = None
+    max_frame_time: float = None
+
+    def update_frame_time(self, frame_time: float):
+        if self.min_frame_time is not None:
+            self.min_frame_time = min(self.min_frame_time, frame_time)
+            self.max_frame_time = max(self.max_frame_time, frame_time)
+        else:
+            self.min_frame_time = frame_time
+            self.max_frame_time = frame_time
 
 
 def _detect_frame_time(xmot: Xmot):
@@ -39,6 +59,122 @@ def _detect_frame_time(xmot: Xmot):
     """
 
 
+def _group_motion_parts(xmot: Xmot) -> list[tuple[MotionPartChunk, list[KeyFrameChunk]]]:
+    motion_parts = []
+    cur_motion_part = None
+    cur_key_frames = []
+
+    def push_motion_part():
+        if cur_motion_part is not None:
+            motion_parts.append((cur_motion_part, cur_key_frames))
+        elif cur_key_frames:
+            raise ValueError('KeyFrame chunk must be preceded by a MotionPart chunk.')
+
+    for chunk in xmot.motion.chunks:
+        if isinstance(chunk, MotionPartChunk):
+            push_motion_part()
+            cur_motion_part = chunk
+            cur_key_frames = []
+        elif isinstance(chunk, KeyFrameChunk):
+            cur_key_frames.append(chunk)
+
+    push_motion_part()
+    return motion_parts
+
+
+def _import_motion_part(chunk: MotionPartChunk, pose_bone: bpy.types.PoseBone, state: _ImportState) \
+        -> tuple[Matrix, Matrix]:
+    # This should be the rest matrix (local to parent).
+    motion_part_pose_matrix = Matrix.LocRotScale(to_blend_vec(chunk.pose_position) * state.root_scale,
+                                                 to_blend_quat(chunk.pose_rotation),
+                                                 to_blend_vec(chunk.pose_scale))
+
+    bone = pose_bone.bone
+    # Rest matrices in xact and pose matrices in the xmot are always relative to the parent bone.
+    # Whereas rest matrices in Blender are absolute and pose matrices in blender are relative to the
+    # rest matrix of the bone.
+    # Therefore, we have to calculate the parent relative bone rest matrix,
+    # and then calculate rest_matrix.inverted() * motion_part_pose_matrix.
+    rest_matrix = bone.matrix_local
+    if bone.parent:
+        rest_matrix = (bone.parent.matrix_local @ bone_correction_matrix_inv).inverted_safe() @ rest_matrix
+    else:
+        rest_matrix = state.root_matrix_no_scale.inverted_safe() @ rest_matrix
+    rest_matrix_inv = rest_matrix.inverted_safe()
+
+    # The symmetry here is O = (B * C)^-1 * ((P * C * C^-1 * X) * C)
+    # with B * C: Absolute Rest matrix (bone.matrix_local)
+    #      C: Correction (bone_correct_matrix),
+    #      P * C: Absolute Parent Rest matrix (bone.parent.matrix_local)
+    #      X: Xmot Pose matrix (motion_part_pose_matrix)
+    #      O: Pose Matrix (pose_bone.matrix_basis)
+    pre_matrix = rest_matrix_inv
+    post_matrix = bone_correction_matrix
+    # Set pose matrix used in xmot (relative to bone's rest matrix).
+    pose_bone.matrix_basis = pre_matrix @ motion_part_pose_matrix @ post_matrix
+
+    return pre_matrix, post_matrix
+
+
+def _import_key_frame_chunk(keyframe_chunk: KeyFrameChunk, pose_bone: bpy.types.PoseBone,
+                            pre_matrix: Matrix, post_matrix: Matrix, state: _ImportState):
+    match keyframe_chunk.animation_type:
+        # Position
+        case AnimationType.Position:
+            curve_path = 'location'
+            value_extract = lambda v: (pre_matrix @ Matrix.Translation(
+                to_blend_vec(v) * state.root_scale) @ post_matrix).to_translation()
+            num_channels = 3
+        # Rotation
+        case AnimationType.Rotation:
+            curve_path = 'rotation_quaternion'
+            value_extract = lambda v: (
+                    pre_matrix @ to_blend_quat(v).to_matrix().to_4x4() @ post_matrix).to_quaternion()
+            num_channels = 4
+        # Scaling
+        case AnimationType.Scaling:
+            curve_path = 'scale'
+            value_extract = lambda v: (pre_matrix @ Matrix.LocRotScale(None, None, to_blend_vec(
+                v)) @ post_matrix).to_scale().to_3d()
+            num_channels = 3
+        case _:
+            raise ValueError(f'Unsupported animation type: {keyframe_chunk.animation_type}')
+
+    match keyframe_chunk.interpolation_type:
+        # Linear
+        case InterpolationType.Linear:
+            interpolation = 'LINEAR'
+        # Bezier
+        case InterpolationType.Bezier:
+            interpolation = 'BEZIER'
+        case _:
+            raise ValueError(f'Unsupported interpolation type: {keyframe_chunk.interpolation_type}')
+
+    prop = pose_bone.path_from_id(curve_path)
+    curves = [state.action.fcurves.new(prop, index=channel, action_group=pose_bone.name)
+              for channel in range(num_channels)]
+
+    # TODO: Cycle vs. Extrapolation
+    # Pre-allocate all keyframes
+    for channel in range(num_channels):
+        curves[channel].keyframe_points.add(len(keyframe_chunk.frames))
+
+    # TODO: Proper time/FPS scaling
+    for i, frame in enumerate(keyframe_chunk.frames):
+        value = value_extract(frame.value)
+
+        for channel in range(num_channels):
+            keyframe_point = curves[channel].keyframe_points[i]
+            keyframe_point.co = frame.time * state.fps, value[channel]
+            keyframe_point.interpolation = interpolation
+
+        state.update_frame_time(frame.time)
+
+    # Usage of low level API to insert key frames requires manual update afterwards.
+    for curve in curves:
+        curve.update()
+
+
 def load_xmot(context: bpy.types.Context, filepath: str, global_scale: float, global_matrix: Matrix,
               ignore_transform: bool):
     name = Path(filepath).stem
@@ -49,8 +185,6 @@ def load_xmot(context: bpy.types.Context, filepath: str, global_scale: float, gl
         raise ValueError("No selected armature found.")
 
     fps = _detect_frame_time(xmot)
-    min_frame_time = None
-    max_frame_time = None
 
     action = bpy.data.actions.new(name)
     action.use_fake_user = True
@@ -67,119 +201,20 @@ def load_xmot(context: bpy.types.Context, filepath: str, global_scale: float, gl
     root_scale, root_matrix_no_scale = calc_arm_root_transformation(arm_obj.matrix_basis, global_scale,
                                                                     global_matrix, ignore_transform)
 
-    last_motion_part = None
-    for chunk in xmot.motion.chunks:
-        # TODO: Should we reset pose matrix (to rest matrix) of bones for which the xmot does not specify a pose matrix?
-        if isinstance(chunk, MotionPartChunk):
-            last_motion_part = chunk.name
-            # This should be the rest matrix (local to parent).
-            motion_part_pose_matrix = Matrix.LocRotScale(to_blend_vec(chunk.pose_position) * root_scale,
-                                                         to_blend_quat(chunk.pose_rotation),
-                                                         to_blend_vec(chunk.pose_scale))
+    state = _ImportState(arm_obj, action, fps, root_scale, root_matrix_no_scale)
 
-            if last_motion_part not in arm_obj.pose.bones:
-                continue
+    for motion_part, key_frames in _group_motion_parts(xmot):
+        if motion_part.name not in arm_obj.pose.bones:
+            logger.warning('Unknown MotionPart, skipping motion: {}', motion_part.name)
+            continue
 
-            pose_bone = arm_obj.pose.bones[last_motion_part]
-            bone = pose_bone.bone
-            # Rest matrices in xact and pose matrices in the xmot are always relative to the parent bone.
-            # Whereas rest matrices in Blender are absolute and pose matrices in blender are relative to the
-            # rest matrix of the bone.
-            # Therefore, we have to calculate the parent relative bone rest matrix,
-            # and then calculate rest_matrix.inverted() * motion_part_pose_matrix.
-            rest_matrix = bone.matrix_local
-            if bone.parent:
-                rest_matrix = (bone.parent.matrix_local @ bone_correction_matrix_inv).inverted_safe() @ rest_matrix
-            else:
-                rest_matrix = root_matrix_no_scale.inverted_safe() @ rest_matrix
-            rest_matrix_inv = rest_matrix.inverted_safe()
+        # Lookup bone
+        pose_bone = arm_obj.pose.bones[motion_part.name]
+        pre_matrix, post_matrix = _import_motion_part(motion_part, pose_bone, state)
+        for key_frame in key_frames:
+            _import_key_frame_chunk(key_frame, pose_bone, pre_matrix, post_matrix, state)
 
-            # The symmetry here is O = (B * C)^-1 * ((P * C * C^-1 * X) * C)
-            # with B * C: Absolute Rest matrix (bone.matrix_local)
-            #      C: Correction (bone_correct_matrix),
-            #      P * C: Absolute Parent Rest matrix (bone.parent.matrix_local)
-            #      X: Xmot Pose matrix (motion_part_pose_matrix)
-            #      O: Pose Matrix (pose_bone.matrix_basis)
-            pre_matrix = rest_matrix_inv
-            post_matrix = bone_correction_matrix
-            # Set pose matrix used in xmot (relative to bone's rest matrix).
-            pose_bone.matrix_basis = pre_matrix @ motion_part_pose_matrix @ post_matrix
-        elif isinstance(chunk, KeyFrameChunk):
-            if last_motion_part is None:
-                raise ValueError('KeyFrame chunk must be preceded by a MotionPart chunk.')
-
-            if last_motion_part not in arm_obj.pose.bones:
-                print('Unknown MotionPart, skipping motion:', last_motion_part)
-                continue
-
-            # Lookup bone
-            pose_bone = arm_obj.pose.bones[last_motion_part]
-
-            keyframe_chunk = chunk
-            match keyframe_chunk.animation_type:
-                # Position
-                case AnimationType.Position:
-                    curve_path = 'location'
-                    value_extract = lambda v: (pre_matrix @ Matrix.Translation(
-                        to_blend_vec(v) * root_scale) @ post_matrix).to_translation()
-                    num_channels = 3
-                # Rotation
-                case AnimationType.Rotation:
-                    curve_path = 'rotation_quaternion'
-                    value_extract = lambda v: (
-                            pre_matrix @ to_blend_quat(v).to_matrix().to_4x4() @ post_matrix).to_quaternion()
-                    num_channels = 4
-                # Scaling
-                case AnimationType.Scaling:
-                    curve_path = 'scale'
-                    value_extract = lambda v: (pre_matrix @ Matrix.LocRotScale(None, None, to_blend_vec(
-                        v)) @ post_matrix).to_scale().to_3d()
-                    num_channels = 3
-                case _:
-                    print('Unsupported animation type:', keyframe_chunk.animation_type)
-                    continue
-
-            match keyframe_chunk.interpolation_type:
-                # Linear
-                case InterpolationType.Linear:
-                    interpolation = 'LINEAR'
-                # Bezier
-                case InterpolationType.Bezier:
-                    interpolation = 'BEZIER'
-                case _:
-                    print('Unsupported interpolation type:', keyframe_chunk.interpolation_type)
-                    continue
-
-            prop = pose_bone.path_from_id(curve_path)
-            curves = [action.fcurves.new(prop, index=channel, action_group=pose_bone.name)
-                      for channel in range(num_channels)]
-
-            # TODO: Cycle vs. Extrapolation
-            # Pre-allocate all keyframes
-            for channel in range(num_channels):
-                curves[channel].keyframe_points.add(len(keyframe_chunk.frames))
-
-            # TODO: Proper time/FPS scaling
-            for i, frame in enumerate(keyframe_chunk.frames):
-                value = value_extract(frame.value)
-
-                for channel in range(num_channels):
-                    keyframe_point = curves[channel].keyframe_points[i]
-                    keyframe_point.co = frame.time * fps, value[channel]
-                    keyframe_point.interpolation = interpolation
-
-                if min_frame_time is not None:
-                    min_frame_time = min(min_frame_time, frame.time)
-                    max_frame_time = max(max_frame_time, frame.time)
-                else:
-                    min_frame_time = frame.time
-                    max_frame_time = frame.time
-
-            # Usage of low level API to insert key frames requires manual update afterwards.
-            for curve in curves:
-                curve.update()
-
-    if min_frame_time is not None:
-        context.scene.frame_start = math.trunc(min_frame_time * fps)
-        context.scene.frame_end = math.ceil(max_frame_time * fps)
+    if state.min_frame_time is not None:
+        context.scene.frame_start = math.trunc(state.min_frame_time * fps)
+        context.scene.frame_end = math.ceil(state.max_frame_time * fps)
         context.scene.frame_current = 0
